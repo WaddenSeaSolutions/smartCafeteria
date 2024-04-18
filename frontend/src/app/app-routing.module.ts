9a1a77c9707d47ea0a989a12e78217ac703fef8f
Andreas Eriksen <pjuske.10@live.dk>
2024-04-18 09:38:56 +0200

    Login page functional

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {LoginPageComponent} from "./login-page/login-page.component";
 
 const routes: Routes = [
   {
@@ -8,8 +9,12 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: '/home',
     pathMatch: 'full'
+  },
+  {
+    path: 'login-page',
+    component: LoginPageComponent
   },
 ];
 
---
+++ frontend/src/app/login-page/login-page.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {environment} from "../../environments/environment";
+
+@Component({
+  selector: 'app-login-page',
+  template: `
+    <div class="background"></div>
+
+    <ion-content style="--background: none; position: absolute; top: 20%">
+      <div style="margin-left: 25%; margin-right: 25%; padding: 2%; border: 1px solid grey ; text-align: center; ">
+        <strong><p>Login side</p></strong>
+        <ion-item>
+          <br>
+          <ion-input style="text-align: center; " placeholder="Brugernavn" [formControl]="username"></ion-input>
+        </ion-item>
+        <br>
+        <ion-item>
+          <ion-input type="password" style="text-align: center;" placeholder="Kodeord" [formControl]="password"></ion-input>
+        </ion-item>
+        <br>
+        <div style="display: flex; justify-content: center;">
+          <ion-button class="btnBackground" style="flex: 1; margin: 3%" (click)="login()">Login</ion-button>
+          <ion-button class="btnBackground" style="flex: 1; margin: 3%" (click)="registerNewUser()">Opret Konto</ion-button>
+        </div>
+      </div>
+    </ion-content>
+
+
+  `,
+  styleUrls: ['./login-page.component.scss'],
+})
+export class LoginPageComponent {
+  username = new FormControl('', Validators.compose([Validators.min(5), Validators.max(20), Validators.required]))
+  password = new FormControl('', Validators.compose([Validators.min(8), Validators.max(30), Validators.required]))
+
+  public checkIfLoggedIn: boolean;
+
+  myFormGroup = new FormGroup({
+    username: this.username,
+    password: this.password,
+  })
+
+  constructor(private http: HttpClient, private router: Router) {
+    this.checkIfLoggedIn = localStorage.getItem('token') != null;
+
+  //   if (this.checkIfLoggedIn){
+  //     this.router.navigate(['home'])
+  //   }
+  }
+
+  login() {
+    if (this.myFormGroup.valid || true) {
+      this.http.post(environment.baseUrl + '/login', this.myFormGroup.value, {responseType: 'text'})
+        .subscribe({
+          next: (response) => {
+            if (response) {
+              // store token
+              localStorage.setItem('token', response);
+              let payload = JSON.parse(atob(response.split(".")[1]))
+              //Store the role, only allows for visual admin controls
+              localStorage.setItem('role', payload.role)
+              //Go to homepage after successful login
+              this.router.navigate(["home"])
+              location.reload();
+            }
+          },
+          error: (err) => {
+            console.error(err);
+          }
+        });
+    }
+  }
+
+  async registerNewUser() {
+    await this.router.navigate(['register'])
+  }
+}
+
