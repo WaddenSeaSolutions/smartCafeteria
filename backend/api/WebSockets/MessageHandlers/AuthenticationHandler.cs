1a8e16cd9fe00f18747050b4a1d7447a9dd4a22d
jonas jakobsen <jonasjakobsen1152@gmail.com>
2024-04-23 17:06:00 +0200

    authentication made simpler
    I realized that I only needed one authenticationHandler and not multiple.

--- backend/api/WebSockets/MessageHandlers/AuthenticationHandler.cs
+++ backend/api/WebSockets/MessageHandlers/AuthenticationHandler.cs
@@ -5,11 +5,11 @@ using Microsoft.IdentityModel.Tokens;
 
 namespace backend.WebSockets.MessageHandlers
 {
-    public class AdminAuthenticationHandler : IMessageHandler
+    public class AuthenticationHandler : IMessageHandler
     {
         private readonly TokenService _tokenService;
 
-        public AdminAuthenticationHandler(TokenService tokenService)
+        public AuthenticationHandler(TokenService tokenService)
         {
             _tokenService = tokenService;
         }
@@ -24,28 +24,22 @@ namespace backend.WebSockets.MessageHandlers
             {
                 // Validate the token and get the user
                 var user = _tokenService.validateTokenAndReturnUser(token);
-
-                // Check if the user is an admin
-                if (user.Role != "admin")
-                {
-                    socket.Send("Unauthorized");
-                    return;
-                }
+                
                 // Update the ConnectionMetadata for this connection
                 var connectionId = Guid.Parse(socket.ConnectionInfo.Id.ToString());
                 if (WebSocketManager._connectionMetadata.TryGetValue(connectionId, out var connectionMetadata))
                 {
                     connectionMetadata.Username = user.Username;
                     connectionMetadata.Authenticated = true;
+                    connectionMetadata.Role = user.Role;
                     connectionMetadata.IsAdmin = user.Role == "admin";
                 }
                 else
                 {
                     Console.WriteLine("Exeption thrown in AdminAuthenticationHandler");
-                    throw new Exception("Connection not found in _connectionMetadata dictionary");
                 }
 
-                socket.Send("Admin authenticated");
+                socket.Send("User Authenticated");
             }
             catch (SecurityTokenException)
             {
