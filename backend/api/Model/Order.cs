584d64e3f86514defc27fad2e86dfa9889aa6edb
Andreas Eriksen <pjuske.10@live.dk>
2024-04-24 10:29:19 +0200

    setting up order

--- backend/api/Model/Order.cs
+++ backend/api/Model/Order.cs
@@ -2,5 +2,11 @@
 
 public class Order
 {
+    public int Id { get; set; }
     
+    public string Timestamp { get; set; }
+    
+    public Boolean Payment { get; set; }
+    
+    public Boolean Done { get; set; }
 }
