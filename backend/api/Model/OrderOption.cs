e19983aae80f9908558e43b1b54398a0118b766c
Andreas Eriksen <pjuske.10@live.dk>
2024-05-14 09:17:36 +0200

    json

--- backend/api/Model/OrderOption.cs
+++ backend/api/Model/OrderOption.cs
@@ -1,11 +1,15 @@
+using System.Text.Json.Serialization;
+
 namespace backend.Model;
 
 public class OrderOption
 {
+    [JsonPropertyName("id")]
     public int Id { get; set; }
-    
+    [JsonPropertyName("optionName")]
     public string OptionName { get; set; }
+    [JsonPropertyName("active")]
     public Boolean Active { get; set; }
-    
+    [JsonPropertyName("deleted")]
     public Boolean Deleted { get; set; }
 }
