263bf966c596be5fb55ba09dd463ec4ff9c08a13
jonas jakobsen <jonasjakobsen1152@gmail.com>
2024-05-15 10:14:52 +0200

    save

---
+++ backend/api/Model/OrderDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace backend.Model;
+
+public class OrderDTO
+{
+    public string Timestamp { get; set; }
+    
+    public Boolean Payment { get; set; }
+    
+    public Boolean Done { get; set; }
+    
+    public Array OrderOptions { get; set; }
+}
