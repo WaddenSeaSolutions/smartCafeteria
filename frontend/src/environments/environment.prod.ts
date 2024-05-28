6295014323c8d8b2eb03b3c3ce6d9a8fc0ac23bb
jonasjakobsen1152 <114471902+jonasjakobsen1152@users.noreply.github.com>
2024-05-28 09:40:23 +0200

    save

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://pcforum.azurewebsites.net'
+  baseUrl: 'ws://smartcafeteriasocket-58246761d167.herokuapp.com/'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  baseUrl:'http://localhost:5000'
+  baseUrl: 'ws://localhost:8181',
+  httpUrl: 'http://localhost:9999'
 };
 
 /*
