e5051080cf9724f05dbd857c1342ffaca59cfbf5
Thomasmp00 <thomaxx00@gmail.com>
2024-05-29 10:57:17 +0200

    make the websocket safe

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'ws://smartcafeteriasocket-58246761d167.herokuapp.com/'
+  baseUrl: 'wss://smartcafeteriasocket-58246761d167.herokuapp.com/'
 };
